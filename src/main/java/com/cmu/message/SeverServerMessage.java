dbfc523a7165330dd3170c543f1fd2932199d467
tianxuq <tianxuq@andrew.cmu.edu>
2021-11-08 18:06:44 -0500

    created new server-server msg class

---
+++ src/main/java/com/cmu/message/SeverServerMessage.java
@@ -0,0 +1,29 @@
+package com.cmu.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author
+ */
+@Data
+@AllArgsConstructor
+public class ServerServerMessage implements Serializable {
+    private String primaryName;
+    private String backupName;
+    private long myState;
+    private Long requestNum;
+    private Direction direction;
+
+    @Override
+    public String toString() {
+        return " <" + getPrimaryName() + ", " + getBackupName() + ", " + getMyState() + ", " + getRequestNum() + ", "
+                + getDirection() + ">";
+    }
+
+    public void incRequestNum() {
+        requestNum++;
+    }
+}
